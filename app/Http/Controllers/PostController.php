ae6243c63dfada430cca3e8da17dab6bd1a69c9b
vallemkr <vallemkr@umich.edu>
2019-12-30 01:36:12 -0500

    added postController instructions for myself

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,13 @@ class PostController extends Controller
      */
     public function formSubmit(Request $request)
     {
+        // make xml api string here from form data recieved
+        // then call xml file
+        // then use a return statement to return and simultaneously json_encode the xml
     	return response()->json([$request->all()]);
     }
+
+
+    // add an onPageLoad function here also, in case user enters specific url
+    // for this function, just use the posted information directly, no need to make the xml api string
 }
